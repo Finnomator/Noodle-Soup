5917e6c0411bcee087f8931431ad87f7bdd853f7
Finn Drünert <finndruenert@gmail.com>
2022-08-28 11:41:54 +0200

    Update

---
+++ CustomIDE/Colors.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections.Generic;
+
+class Colors {
+    public Dictionary<string, string> colors = new Dictionary<string, string>();
+    public string[] keywords = {
+        "False",
+        "None",
+        "True",
+        "and",
+        "as",
+        "assert",
+        "async",
+        "await",
+        "break",
+        "class",
+        "continue",
+        "def",
+        "del",
+        "elif",
+        "else",
+        "except",
+        "finally",
+        "for",
+        "from",
+        "global",
+        "if",
+        "import",
+        "in",
+        "is",
+        "lambda",
+        "nonlocal",
+        "not",
+        "or",
+        "pass",
+        "raise",
+        "return",
+        "try",
+        "while",
+        "with",
+        "yield"
+    };
+    public string keywords_color = "#cc00ff";
+
+    public Colors() {
+        foreach (string keyword in keywords) {
+            colors[keyword] = keywords_color;
+        }
+        colors["default"] = "#ffffff";
+    }
+}
--- CustomIDE/MainWindow.xaml.cs
+++ CustomIDE/MainWindow.xaml.cs
@@ -14,38 +14,85 @@ using System.Windows.Navigation;
 using System.Windows.Shapes;
 using Microsoft.Win32;
 using System.IO;
+using System.Diagnostics;
+using System.Threading;
 
 namespace CustomIDE {
     public partial class MainWindow : Window {
+
+        BrushConverter bc;
+        string typing_word = "";
+        bool last_was_colored = false;
+        Dictionary<string, string> colors;
+        Key[] word_end_chars = { Key.OemPeriod, Key.OemOpenBrackets, Key.OemCloseBrackets, Key.OemComma, Key.OemQuotes, Key.OemSemicolon, Key.Space, Key.Return };
         public MainWindow() {
             InitializeComponent();
-
-            AppendText(CodeBox, "Test", "Red");
+            bc = new BrushConverter();
+            colors = new Colors().colors;
         }
 
         void AppendText(RichTextBox box, string text, string color) {
-            BrushConverter bc = new BrushConverter();
             TextRange tr = new TextRange(box.Document.ContentEnd, box.Document.ContentEnd);
             tr.Text = text;
-            try {
-                tr.ApplyPropertyValue(TextElement.ForegroundProperty,
-                    bc.ConvertFromString(color));
-            } catch (FormatException) { }
+            tr.ApplyPropertyValue(TextElement.ForegroundProperty, bc.ConvertFromString(color));
         }
 
         private void OpenFileClick(object sender, RoutedEventArgs e) {
             OpenFileDialog openFileDialog = new OpenFileDialog();
-            //if (openFileDialog.ShowDialog() == true)
-                //CodeBox. = File.ReadAllText(openFileDialog.FileName);
+            TextRange tr = new TextRange(CodeBox.Document.ContentStart, CodeBox.Document.ContentEnd);
+            if ((bool)!openFileDialog.ShowDialog())
+                return;
+
+            tr.Text = File.ReadAllText(openFileDialog.FileName);
         }
 
         private void OpenDirClick(object sender, RoutedEventArgs e) {
-            
+
         }
 
         private void SaveClick(object sender, RoutedEventArgs e) {
 
         }
 
+        void ColorWord() {
+            if (typing_word == "")
+                return;
+            object color = bc.ConvertFromString(colors[colors.ContainsKey(typing_word) ? typing_word : "default"]);
+            TextRange tr = new TextRange(CodeBox.CaretPosition.GetPositionAtOffset(-typing_word.Length), CodeBox.CaretPosition);
+            tr.ApplyPropertyValue(TextElement.ForegroundProperty, color);
+        }
+
+        TextRange FindWordFromPos(TextPointer pos) {
+            TextRange tr;
+            
+            tr = new TextRange(CodeBox.CaretPosition.GetPositionAtOffset(-1), pos);
+            Debug.WriteLine(tr.Text);
+            return tr;
+        }
+
+        private void KeyDown(object sender, KeyEventArgs e) {
+            if (word_end_chars.Contains(e.Key)) {
+                typing_word = "";
+            } else if (e.Key == Key.Back) {
+                if (typing_word != "")
+                    typing_word = typing_word.Remove(typing_word.Length - 1);
+                else {
+                    TextRange tr = FindWordFromPos(CodeBox.CaretPosition);
+                    typing_word = tr.Text;
+                }
+            } else if (e.Key.ToString().Length != 1) {
+                return;
+            } else {
+                string letter = e.Key.ToString();
+
+                if (!(Keyboard.IsKeyDown(Key.LeftShift) || Keyboard.IsKeyDown(Key.RightShift))) {
+                    letter = letter.ToLower();
+                }
+                typing_word += letter;
+            }
+
+            Debug.WriteLine("\"" + typing_word + "\"");
+            ColorWord();
+        }
     }
 }
